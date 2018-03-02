282b56c6607d1f011ff6ada1c109047e1e15677a
YoProgrammer <yuttasakcom@gmail.com>
2018-03-02 21:12:31 +0700

    create plugin vue-socketio

---
+++ components/email/SendMail.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <button @click="$socket.emit('sendMail', email)">Send Email</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: {
+        from: 'test@test.com',
+        to: 'example@example.com',
+        title: 'test',
+        message: 'test message'
+      }
+    }
+  }
+}
+</script>
+
--- plugins/vue-socketio.js
+++ plugins/vue-socketio.js
@@ -1,9 +1,5 @@
 import Vue from 'vue'
+import VueSocketio from 'vue-socket.io'
+import socketio from 'socket.io-client'
 
-import io from 'socket.io-client'
-
-const socket = io('http://localhost:3000')
-
-socket.on('connect', () => {
-  console.log('connect server')
-})
+Vue.use(VueSocketio, socketio('http://localhost:3000'))
